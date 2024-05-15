722212e881857e864699945bbaeee699e35ca47c
Cooper <jccooper20@gmail.com>
2024-05-14 19:22:30 -0500

    Gonna go ahead and push this since vscode was not recognizing that i made any changes to the code unless i manually saved the file and i do not want to get tricked by that again

--- index.js
+++ index.js
@@ -12,14 +12,27 @@ async function getResult() {
     const userInput = await getUserInput();
     const type = userInput[0];
     const subject = userInput[1];
+    console.log(`Searching for ${type} ${subject}`)
     const response = await vlrSearch(type, subject);
     const $searchPage = cheerio.load(response);
     const pageLink = $searchPage('a.wf-module-item.search-item.mod-first').attr('href');
-    const teamPage = await axios.get(`https://www.vlr.gg${pageLink}`);
-    const $teamPage = cheerio.load(teamPage.data);
+    let subjectResponse;
+    if (type === 'team'){
+         subjectResponse = await getTeamPage(pageLink);
+    } else if (type === 'player'){
+         subjectResponse = await getPlayerPage(pageLink);
+    }
+    else {
+        console.log('Invalid search type. Please try again, this time using either team or player');
+    }
+    $subjectPage = cheerio.load(subjectResponse);
+    
 }
 
 async function getUserInput() {
+    let answered1 = false;
+    let answered2 = false;
+    let answers = [];
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout,
@@ -27,19 +40,37 @@ async function getUserInput() {
     
     const question = (query) => new Promise(resolve => rl.question(query, resolve));
 
-    const type = await question('Enter search type (team/player): ');
-    if (type !== 'team' && type !== 'player') {
-        console.log('Invalid search type. Please try again, this time using either team or player');
-    } else { 
-        const subject = await question('Enter search subject: ');
+    while (!answered1) {
+        const type = await question('Enter search type, either team or player: ');
+        if (type !== 'team' && type !== 'player') {
+            console.log('Invalid search type. Please try again, this time using either team or player');
+        } else { 
+            answered1 = true;
+            answers.push(type);
+        }    
     }
-
+    while(!answered2 && answered1) {
+        const subject = await question('Enter search subject (player/team name): ');
+        if (subject === '') {
+            console.log('Invalid search subject. Please try again.');
+        } else {
+            answered2 = true;
+            answers.push(subject);
+        }
+    }
+    
     rl.close();
-    return [type, subject];
+    return answers;
 }
 
-async function getPlayerMatches(player) {
+async function getPlayerPage(pageLink) {
+    const playerPage = await axios.get(`https://www.vlr.gg${pageLink}`);
+    return playerPage.data;
+}
 
+async function getTeamPage(pageLink) {
+    const teamPage = await axios.get(`https://www.vlr.gg${pageLink}`);
+    return teamPage.data;
 }
 
 getResult();
